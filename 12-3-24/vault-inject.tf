3dbd0446c9998e0a51f4531b11d0850a980d1596
mahi <mahima2071987@gmail.com>
2024-12-03 04:39:04 +0530

    Create vault-inject.tf

---
+++ 12-3-24/vault-inject.tf
@@ -0,0 +1,18 @@
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config" # Path to your kubeconfig file
+  }
+}
+
+resource "helm_release" "vault" {
+  name       = "vault"
+  namespace  = "vault"
+  chart      = "vault"
+  repository = "https://helm.releases.hashicorp.com" # Directly specify the repository URL
+
+  create_namespace = true
+
+  values = [
+    file("values.yaml") # Provide the path to your values.yaml file
+  ]
+}
