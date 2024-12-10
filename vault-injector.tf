32ca418826ce84f798ad81a23efe8b0547ec9767
mahi <mahima2071987@gmail.com>
2024-12-11 01:09:24 +0530

    Update vault-injector.tf

--- vault-injector.tf
+++ vault-injector.tf
@@ -1,47 +1,22 @@
-provider "google" {
-  project = var.project_id
-  region  = var.region
-}
 
-provider "google-beta" {
-  project = var.project_id
-  region  = var.region
-}
-
-data "google_container_cluster" "my_cluster" {
-  name   = var.cluster_name
-  region = var.region
-}
-
-provider "kubernetes" {
-  host = data.google_container_cluster.my_cluster.endpoint
-
-  client_certificate = base64decode(data.google_container_cluster.my_cluster.master_auth[0].client_certificate)
-  client_key         = base64decode(data.google_container_cluster.my_cluster.master_auth[0].client_key)
-  cluster_ca_certificate = base64decode(data.google_container_cluster.my_cluster.master_auth[0].cluster_ca_certificate)
-}
 
 provider "helm" {
   kubernetes {
-    host                   = data.google_container_cluster.my_cluster.endpoint
-    client_certificate     = base64decode(data.google_container_cluster.my_cluster.master_auth[0].client_certificate)
-    client_key             = base64decode(data.google_container_cluster.my_cluster.master_auth[0].client_key)
-    cluster_ca_certificate = base64decode(data.google_container_cluster.my_cluster.master_auth[0].cluster_ca_certificate)
+    config_path = "~/.kube/config" # Path to your kubeconfig file
   }
 }
 
-resource "kubernetes_namespace" "vault" {
-  metadata {
-    name = "vault"
-  }
-}
-
-resource "helm_release" "vault_injector" {
+resource "helm_release" "vault" {
   name       = "vault"
-  namespace  = kubernetes_namespace.vault.metadata[0].name
-  repository = "https://helm.releases.hashicorp.com"
+  namespace  = "vault"
   chart      = "vault"
-  version    = "1.19.0"
+  repository = "https://helm.releases.hashicorp.com" # Directly specify the repository URL
 
-  values = [file("${path.module}/values.yaml")]
+  create_namespace = true
+
+
+  values = [
+    file("../../go-app/values2.yaml")
+  ]
 }
+
